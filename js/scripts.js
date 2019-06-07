6eadc34934d7fb840ccc483a16aa4b3e25f888c9
Emmanuel Muchiri <Emmanuel.Muchiri@outlook.com>
2019-06-07 16:48:58 +0300

    Added the toggles to all the id's in the hide class

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
   $(".hide p").hide();
-  $(".hide").click(function() {
-    $("#design p").fadeToggle();
-    $("#design img").fadeToggle();
+  $("#design").click(function() {
+    $("#design p").toggle();
+    $("#design img").toggle();
+  });
+  $("#dev").click(function() {
+    $("#dev p").toggle();
+    $("#dev img").toggle();
+  });
+  $("#products").click(function() {
+    $("#products p").toggle();
+    $("#products img").toggle();
   });
 });
