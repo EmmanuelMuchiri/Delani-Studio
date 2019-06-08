7c2597e80b459f7e1bc1240068aae8a9ad187fc4
Emmanuel Muchiri <Emmanuel.Muchiri@outlook.com>
2019-06-08 22:27:40 +0300

    added my javascript validation so as not to accept any empty strings or null values

--- js/scripts.js
+++ js/scripts.js
@@ -13,3 +13,19 @@ $(document).ready(function() {
     $("#products img").toggle();
   });
 });
+
+var submitItems = function(name, email, message) {
+
+  var userName = document.getElementById('name').value
+  var userEmail = document.getElementById('email').value
+  var userMessage = document.getElementById('message').value
+  var inputValues = null;
+  // var items = [userName, userEmail, userEmail];
+
+  if (!userName || !userEmail || !userMessage) {
+    alert("Cannot insert empty fields please type again");
+  } else {
+    alert("Your Inputs have been submitted successfully");
+  }
+  return false;
+}
